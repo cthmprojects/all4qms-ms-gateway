24f9afea77ab983724e022f35b42b6ba17029ad0
costamarcos <marcosvinicius.mecatronica@gmail.com>
2023-10-04 22:00:53 -0400

    feat (usuario): list users

--- src/main/webapp/app/entities/usuario/usuario.tsx
+++ src/main/webapp/app/entities/usuario/usuario.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { Button, Table } from 'reactstrap';
+import { Button, Row, Table } from 'reactstrap';
 import { Translate, TextFormat, getSortState, JhiPagination, JhiItemCount } from 'react-jhipster';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -12,6 +12,21 @@ import { useAppDispatch, useAppSelector } from 'app/config/store';
 import { IUsuario } from 'app/shared/model/usuario.model';
 import { getEntities } from './usuario.reducer';
 import GeneralList from 'app/shared/layout/list/general-list';
+import {
+  Box,
+  Breadcrumbs,
+  CircularProgress,
+  Divider,
+  Pagination,
+  Paper,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@mui/material';
+import './usuario.scss';
 
 export const Usuario = () => {
   const dispatch = useAppDispatch();
@@ -82,138 +97,99 @@ export const Usuario = () => {
     sortEntities();
   };
 
+  const columns = ['Nome', 'E-mail', 'Função', 'Gestor', 'Setor'];
+
+  const renderTable = () => {
+    if (columns.length > 0 && usuarioList.length > 0) {
+      return (
+        <>
+          <TableContainer component={Paper} style={{ marginTop: '30px', boxShadow: 'none' }}>
+            <Table sx={{ width: '100%' }}>
+              <TableHead>
+                <TableRow>
+                  {columns.map(col => (
+                    <TableCell align="left">{col}</TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {usuarioList.map(usuario => (
+                  <TableRow>
+                    <TableCell>{usuario.nome}</TableCell>
+                    <TableCell>{usuario.email}</TableCell>
+                    <TableCell>{usuario.funcao.nome}</TableCell>
+                    <TableCell>{usuario.gestor.nome}</TableCell>
+                    <TableCell>{usuario.setor.nome}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <div className="justify-content-center d-flex">
+            <JhiPagination
+              activePage={paginationState.activePage}
+              onSelect={handlePagination}
+              maxButtons={5}
+              itemsPerPage={paginationState.itemsPerPage}
+              totalItems={totalItems}
+            />
+          </div>
+        </>
+      );
+    } else {
+      return (
+        <Row className="justify-content-center mt-5">
+          <span style={{ color: '#7d7d7d' }}>Nenhum item encontrado.</span>
+        </Row>
+      );
+    }
+  };
+
   return (
-    <GeneralList />
-    // <div>
-    //   <h2 id="usuario-heading" data-cy="UsuarioHeading">
-    //     Usuarios
-    //     <div className="d-flex justify-content-end">
-    //       <Button className="me-2" color="info" onClick={handleSyncList} disabled={loading}>
-    //         <FontAwesomeIcon icon="sync" spin={loading} /> Atualizar lista
-    //       </Button>
-    //       <Link to="/usuario/new" className="btn btn-primary jh-create-entity" id="jh-create-entity" data-cy="entityCreateButton">
-    //         <FontAwesomeIcon icon="plus" />
-    //         &nbsp; Criar novo Usuario
-    //       </Link>
-    //     </div>
-    //   </h2>
-    //   <div className="table-responsive">
-    //     {usuarioList && usuarioList.length > 0 ? (
-    //       <Table responsive>
-    //         <thead>
-    //           <tr>
-    //             <th className="hand" onClick={sort('id')}>
-    //               ID <FontAwesomeIcon icon="sort" />
-    //             </th>
-    //             <th className="hand" onClick={sort('nome')}>
-    //               Nome <FontAwesomeIcon icon="sort" />
-    //             </th>
-    //             <th className="hand" onClick={sort('email')}>
-    //               Email <FontAwesomeIcon icon="sort" />
-    //             </th>
-    //             <th className="hand" onClick={sort('isGestor')}>
-    //               Is Gestor <FontAwesomeIcon icon="sort" />
-    //             </th>
-    //             <th className="hand" onClick={sort('criadoEm')}>
-    //               Criado Em <FontAwesomeIcon icon="sort" />
-    //             </th>
-    //             <th className="hand" onClick={sort('atualizadoEm')}>
-    //               Atualizado Em <FontAwesomeIcon icon="sort" />
-    //             </th>
-    //             <th>
-    //               Funcao <FontAwesomeIcon icon="sort" />
-    //             </th>
-    //             <th>
-    //               Gestor <FontAwesomeIcon icon="sort" />
-    //             </th>
-    //             <th>
-    //               Setor <FontAwesomeIcon icon="sort" />
-    //             </th>
-    //             <th>
-    //               User <FontAwesomeIcon icon="sort" />
-    //             </th>
-    //             <th>
-    //               Criado Por <FontAwesomeIcon icon="sort" />
-    //             </th>
-    //             <th>
-    //               Atualizado Por <FontAwesomeIcon icon="sort" />
-    //             </th>
-    //             <th />
-    //           </tr>
-    //         </thead>
-    //         <tbody>
-    //           {usuarioList.map((usuario, i) => (
-    //             <tr key={`entity-${i}`} data-cy="entityTable">
-    //               <td>
-    //                 <Button tag={Link} to={`/usuario/${usuario.id}`} color="link" size="sm">
-    //                   {usuario.id}
-    //                 </Button>
-    //               </td>
-    //               <td>{usuario.nome}</td>
-    //               <td>{usuario.email}</td>
-    //               <td>{usuario.isGestor ? 'true' : 'false'}</td>
-    //               <td>{usuario.criadoEm ? <TextFormat type="date" value={usuario.criadoEm} format={APP_DATE_FORMAT} /> : null}</td>
-    //               <td>{usuario.atualizadoEm ? <TextFormat type="date" value={usuario.atualizadoEm} format={APP_DATE_FORMAT} /> : null}</td>
-    //               <td>{usuario.funcao ? <Link to={`/funcao/${usuario.funcao.id}`}>{usuario.funcao.nome}</Link> : ''}</td>
-    //               <td>{usuario.gestor ? <Link to={`/usuario/${usuario.gestor.id}`}>{usuario.gestor.nome}</Link> : ''}</td>
-    //               <td>{usuario.setor ? <Link to={`/setor/${usuario.setor.id}`}>{usuario.setor.nome}</Link> : ''}</td>
-    //               <td>{usuario.user ? usuario.user.login : ''}</td>
-    //               <td>{usuario.criadoPor ? <Link to={`/usuario/${usuario.criadoPor.id}`}>{usuario.criadoPor.nome}</Link> : ''}</td>
-    //               <td>
-    //                 {usuario.atualizadoPor ? <Link to={`/usuario/${usuario.atualizadoPor.id}`}>{usuario.atualizadoPor.nome}</Link> : ''}
-    //               </td>
-    //               <td className="text-end">
-    //                 <div className="btn-group flex-btn-group-container">
-    //                   <Button tag={Link} to={`/usuario/${usuario.id}`} color="info" size="sm" data-cy="entityDetailsButton">
-    //                     <FontAwesomeIcon icon="eye" /> <span className="d-none d-md-inline">Visualizar</span>
-    //                   </Button>
-    //                   <Button
-    //                     tag={Link}
-    //                     to={`/usuario/${usuario.id}/edit?page=${paginationState.activePage}&sort=${paginationState.sort},${paginationState.order}`}
-    //                     color="primary"
-    //                     size="sm"
-    //                     data-cy="entityEditButton"
-    //                   >
-    //                     <FontAwesomeIcon icon="pencil-alt" /> <span className="d-none d-md-inline">Editar</span>
-    //                   </Button>
-    //                   <Button
-    //                     tag={Link}
-    //                     to={`/usuario/${usuario.id}/delete?page=${paginationState.activePage}&sort=${paginationState.sort},${paginationState.order}`}
-    //                     color="danger"
-    //                     size="sm"
-    //                     data-cy="entityDeleteButton"
-    //                   >
-    //                     <FontAwesomeIcon icon="trash" /> <span className="d-none d-md-inline">Excluir</span>
-    //                   </Button>
-    //                 </div>
-    //               </td>
-    //             </tr>
-    //           ))}
-    //         </tbody>
-    //       </Table>
-    //     ) : (
-    //       !loading && <div className="alert alert-warning">Nenhum Usuario encontrado</div>
-    //     )}
-    //   </div>
-    //   {totalItems ? (
-    //     <div className={usuarioList && usuarioList.length > 0 ? '' : 'd-none'}>
-    //       <div className="justify-content-center d-flex">
-    //         <JhiItemCount page={paginationState.activePage} total={totalItems} itemsPerPage={paginationState.itemsPerPage} />
-    //       </div>
-    //       <div className="justify-content-center d-flex">
-    //         <JhiPagination
-    //           activePage={paginationState.activePage}
-    //           onSelect={handlePagination}
-    //           maxButtons={5}
-    //           itemsPerPage={paginationState.itemsPerPage}
-    //           totalItems={totalItems}
-    //         />
-    //       </div>
-    //     </div>
-    //   ) : (
-    //     ''
-    //   )}
-    // </div>
+    <>
+      {usuarioList.lenght > 0 ? (
+        <Box
+          sx={{
+            display: 'flex',
+            width: '100vw',
+            height: '100vh',
+            background: '#c6c6c6',
+            zIndex: 15,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <CircularProgress size={80} />
+        </Box>
+      ) : (
+        <div className="padding-container">
+          <div className="container-style">
+            <Breadcrumbs aria-label="breadcrumb">
+              <Link style={{ textDecoration: 'none', color: '#49a7ea', fontWeight: 400 }} to={'/'}>
+                Home
+              </Link>
+              <Typography className="link">Usuários</Typography>
+            </Breadcrumbs>
+            <h1 className="title">Usuários</h1>
+            <div style={{ paddingBottom: '30px' }}>
+              <Button
+                variant="contained"
+                className="primary-button"
+                style={{ marginRight: '10px' }}
+                onClick={() => navigate('/usuario/new')}
+              >
+                CADASTRAR
+              </Button>
+              <Button variant="contained" className="update-button" onClick={() => getAllEntities()}>
+                ATUALIZAR
+              </Button>
+            </div>
+            <Divider sx={{ borderColor: '#7d7d7d' }}></Divider>
+            {renderTable()}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
