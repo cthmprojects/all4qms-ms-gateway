013fa1d833f7db1622aa5e4e6e5a1a361765cb34
costamarcos <marcosvinicius.mecatronica@gmail.com>
2023-12-14 20:13:39 -0400

    feat: RNC implementation verification

--- src/main/webapp/app/modules/rnc/index.tsx
+++ src/main/webapp/app/modules/rnc/index.tsx
@@ -4,6 +4,9 @@ import { Route } from 'react-router-dom';
 import RncList from './home/rnc-list';
 import RNCNew from './new/rnc-new';
 import GeneralRegister from './new/register-types/general-register/general-register';
+import RegisterImplementation from './new/register-types/register-implementation/register-implementation';
+import RegisterImplementationVerification from './new/register-types/register-implementation-verification/register-implementation-verification';
+import RegisterImplementationClose from './new/register-types/register-implementation-close/register-implementation-close';
 
 const RncRoutes = () => (
   <div>
@@ -11,6 +14,9 @@ const RncRoutes = () => (
       <Route path="" element={<RncList />} />
       <Route path="new" element={<RNCNew />} />
       <Route path="general" element={<GeneralRegister />}></Route>
+      <Route path="general/implementacao" element={<RegisterImplementation />} />
+      <Route path="general/implementacao/validacao" element={<RegisterImplementationVerification />} />
+      <Route path="general/implementacao/fechamento" element={<RegisterImplementationClose />} />
     </ErrorBoundaryRoutes>
   </div>
 );
