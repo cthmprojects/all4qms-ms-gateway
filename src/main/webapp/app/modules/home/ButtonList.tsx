618b79e43406dd7903fb1016cbbd22f1b7d9d9ac
costamarcos <marcosvinicius.mecatronica@gmail.com>
2024-01-21 11:53:32 -0400

    fix: button list

--- src/main/webapp/app/modules/home/ButtonList.tsx
+++ src/main/webapp/app/modules/home/ButtonList.tsx
@@ -5,13 +5,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const ButtonList = () => {
   const buttons = [
-    { icon: <FontAwesomeIcon icon="calendar-check" />, link: '/pagina1', legenda: 'Objetivos e Metas' },
-    { icon: <FontAwesomeIcon icon="file-lines" />, link: '/infodoc', legenda: 'Informações documentadas' },
-    { icon: <FontAwesomeIcon icon="crosshairs" />, link: '/pagina3', legenda: 'Riscos e Oportunidades' },
-    { icon: <FontAwesomeIcon icon="location-crosshairs" />, link: '/pagina3', legenda: 'Auditorias' },
+    { icon: <FontAwesomeIcon icon="calendar-check" />, link: '', legenda: 'Objetivos e Metas', disabled: true },
+    { icon: <FontAwesomeIcon icon="file-lines" />, link: '', legenda: 'Informações documentadas', disabled: true },
+    { icon: <FontAwesomeIcon icon="crosshairs" />, link: '', legenda: 'Riscos e Oportunidades', disabled: true },
+    { icon: <FontAwesomeIcon icon="location-crosshairs" />, link: '', legenda: 'Auditorias', disabled: true },
     { icon: <FontAwesomeIcon icon="file-circle-check" />, link: '/rnc', legenda: 'RNC / OM' },
-    { icon: <FontAwesomeIcon icon="file-circle-exclamation" />, link: '/pagina3', legenda: 'Indicadores de desempenho' },
-    { icon: <FontAwesomeIcon icon="chart-line" />, link: '/pagina3', legenda: 'Análise crítica' },
+    { icon: <FontAwesomeIcon icon="file-circle-exclamation" />, link: '', legenda: 'Indicadores de desempenho', disabled: true },
+    { icon: <FontAwesomeIcon icon="chart-line" />, link: '', legenda: 'Análise crítica', disabled: true },
   ];
 
   return (
@@ -19,8 +19,7 @@ const ButtonList = () => {
       {buttons.map((button, index) => (
         // eslint-disable-next-line react/jsx-key
         <div className="container">
-          <a key={index} className="button" href={button.link}>
-            {/* <Icon icon={button.icon} className="icon" /> */}
+          <a key={index} className={button.disabled ? 'button-disabled' : 'button'} href={button.link} aria-disabled>
             {button.icon}
           </a>
           <span>{button.legenda}</span>
