cc2271b1d9f6f52602ee66a18a9aa466652a849e
costamarcos <marcosvinicius.mecatronica@gmail.com>
2024-05-16 17:50:27 -0400

    feat: infodoc data screen

--- src/main/webapp/app/modules/infodoc/index.tsx
+++ src/main/webapp/app/modules/infodoc/index.tsx
@@ -4,18 +4,27 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import InfodocList from './ui/home/infodoc-list';
 import UploadInfoFile from './ui/forms/upload-files';
+import NewDocument from './new/new-document';
+import getStore from 'app/config/store';
+import { ReducersMapObject, combineReducers } from 'redux';
+import rncReducer from '../rnc/reducers';
 
 const handleTela = (tela: string) => {
   console.log(tela);
 };
 
-const InfodocRoutes = () => (
-  <div>
-    <ErrorBoundaryRoutes>
-      <Route path="" element={<InfodocList />} />
-      <Route path="upload-file" element={<UploadInfoFile />} />
-    </ErrorBoundaryRoutes>
-  </div>
-);
+const InfodocRoutes = () => {
+  const store = getStore();
+  store.injectReducer('all4qmsmsgateway', combineReducers(rncReducer as ReducersMapObject));
+  return (
+    <div>
+      <ErrorBoundaryRoutes>
+        <Route path="" element={<InfodocList />} />
+        <Route path="upload-file" element={<UploadInfoFile />} />
+        <Route path="upload-file/new" element={<NewDocument />} />
+      </ErrorBoundaryRoutes>
+    </div>
+  );
+};
 
 export default InfodocRoutes;
