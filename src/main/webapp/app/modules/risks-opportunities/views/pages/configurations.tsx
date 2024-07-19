9e97ca6f9305ff5c1e32be8179732a5847350af8
Douglas Queiroz <douglas_doug5040@hotmail.com>
2024-07-19 18:11:07 -0400

    feat: ui tela configuracoes com graus

---
+++ src/main/webapp/app/modules/risks-opportunities/views/pages/configurations.tsx
@@ -0,0 +1,43 @@
+import { Box, Breadcrumbs, Tab, Tabs, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { TabsCustom } from '../../../../shared/components/tabs';
+import ConfigurationsTabRisk from '../components/configurations-tab-risk';
+
+const tabsPanel = [
+  {
+    label: 'RISCOS',
+    children: <ConfigurationsTabRisk />,
+  },
+  {
+    label: 'OPORTUNIDADES',
+    children: <ConfigurationsTabRisk />,
+  },
+];
+const Configurations = () => {
+  const [valueTab, setValueTab] = useState(0);
+
+  const handleChangeTab = (_event: React.SyntheticEvent, newValue: number) => {
+    setValueTab(newValue);
+  };
+
+  return (
+    <div className="padding-container">
+      <div className="container-style">
+        <Breadcrumbs aria-label="breadcrumb">
+          <Link to={'/'} style={{ textDecoration: 'none', color: '#49a7ea', fontWeight: 400 }}>
+            Home
+          </Link>
+          <Link to={'/risks-opportunities'} style={{ textDecoration: 'none', color: '#49a7ea', fontWeight: 400 }}>
+            Riscos & Oportunidades
+          </Link>
+          <Typography className="link">Configurações</Typography>
+        </Breadcrumbs>
+
+        <TabsCustom tabsPanel={tabsPanel} valueTab={valueTab} setValueTab={setValueTab} handleChange={handleChangeTab} />
+      </div>
+    </div>
+  );
+};
+
+export default Configurations;
