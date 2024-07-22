269cdff01c724aa2ad77ca61e657fc123fe5c838
Erickson Alves <erickson.higor@gmail.com>
2024-07-22 19:45:22 -0400

    feat: Adiciona home
    Signed-off-by: Erickson Alves <erickson.higor@gmail.com>

--- src/main/webapp/app/modules/risks-opportunities/index.tsx
+++ src/main/webapp/app/modules/risks-opportunities/index.tsx
@@ -1,13 +1,19 @@
+import getStore from 'app/config/store';
 import ErrorBoundaryRoutes from 'app/shared/error/error-boundary-routes';
 import React from 'react';
 import { Route } from 'react-router-dom';
-import { AddOpportunity, AddRisk, EditOpportunity, EditRisk, ViewOpportunity, ViewRisk, Configurations } from './views';
+import { combineReducers, ReducersMapObject } from 'redux';
+import rncReducers from '../rnc/reducers';
+import { AddOpportunity, AddRisk, Configurations, EditOpportunity, EditRisk, Home, ViewOpportunity, ViewRisk } from './views';
 
 const RisksOpportunitiesRoutes = () => {
+  const store = getStore();
+  store.injectReducer('all4qmsmsgatewayrnc', combineReducers(rncReducers as ReducersMapObject));
+
   return (
     <>
       <ErrorBoundaryRoutes>
-        <Route path="" element={<>Home</>} />
+        <Route path="" element={<Home />} />
         <Route path="risk" element={<AddRisk />} />
         <Route path="opportunity" element={<AddOpportunity />} />
         <Route path="risk/:id" element={<EditRisk />} />
