b4d1dd8bc7d5ca19255cb964770c3fc91225732b
Victor Freire <victor.desenvolvedor@gmail.com>
2024-07-25 08:55:57 -0400

    Envio de email ao criar usuário

---
+++ src/main/java/com/tellescom/all4qms/config/MailSenderConfiguration.java
@@ -0,0 +1,28 @@
+package com.tellescom.all4qms.config;
+
+import java.util.Properties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailSenderConfiguration {
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("all4qms.mail.teste@gmail.com");
+        // Use a senha do aplicativo gerada em vez da senha da conta
+        mailSender.setPassword("qqsy sjji klem rigg");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        return mailSender;
+    }
+}
