4082583a1312f3eee3152b41f43bfaaeff8eda04
Erickson Alves <erickson.higor@gmail.com>
2024-07-27 12:24:09 -0400

    feat: Configura reducer
    Signed-off-by: Erickson Alves <erickson.higor@gmail.com>

--- src/main/webapp/app/modules/risks-opportunities/index.tsx
+++ src/main/webapp/app/modules/risks-opportunities/index.tsx
@@ -1,14 +1,15 @@
 import getStore from 'app/config/store';
 import ErrorBoundaryRoutes from 'app/shared/error/error-boundary-routes';
-import React from 'react';
 import { Route } from 'react-router-dom';
 import { combineReducers, ReducersMapObject } from 'redux';
 import rncReducers from '../rnc/reducers';
+import risksOpportunitiesReducers from './reducers';
 import { AddOpportunity, AddRisk, Configurations, EditOpportunity, EditRisk, Home, ViewOpportunity, ViewRisk } from './views';
 
 const RisksOpportunitiesRoutes = () => {
   const store = getStore();
   store.injectReducer('all4qmsmsgatewayrnc', combineReducers(rncReducers as ReducersMapObject));
+  store.injectReducer('all4qmsmsgatewayro', combineReducers(risksOpportunitiesReducers as ReducersMapObject));
 
   return (
     <>
