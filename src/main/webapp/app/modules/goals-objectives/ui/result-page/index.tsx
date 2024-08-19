62bcaa8633a7839edcc8a2a7cf9bb8b5f961ea44
Sandro Haiden Teixeira <sandrohc2010@hotmail.com>
2024-08-19 01:23:02 -0400

    feat: initial integration for meta-result

--- src/main/webapp/app/modules/goals-objectives/ui/result-page/index.tsx
+++ src/main/webapp/app/modules/goals-objectives/ui/result-page/index.tsx
@@ -1,33 +1,39 @@
 import { Box, Breadcrumbs, Button, Fab, FormControl, FormControlLabel, Switch, TextField, Typography } from '@mui/material';
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
-import { UseFormReturn, useForm, useWatch } from 'react-hook-form';
-import DatePicker from 'react-datepicker';
+import { Controller, UseFormReturn, useForm, useWatch } from 'react-hook-form';
 import { QueryClient, useMutation, useQuery } from '@tanstack/react-query';
-import axios from 'axios';
 import AddIcon from '@mui/icons-material/Add';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { toast } from 'react-toastify';
+import { createResult, getMeta, getMetaResultAttatchment, getMetaResults } from '../../goal-result.service';
+import { MaterialDatepicker } from 'app/shared/components/input/material-datepicker';
+import { AttachmentButton } from 'app/shared/layout/AttachmentButton';
 
 type ResultItemProps = {
   save: (formMethods: UseFormReturn<any>) => void;
+  initialPayload?: any;
 };
 
-const createResult = async (payload: any) => {
-  const { data } = await axios.post<any[]>('/urlalguma', payload);
-  return data;
-};
+const queryClient = new QueryClient();
 
-const ResultItem = ({ save }: ResultItemProps) => {
+const ResultItem = ({ save, initialPayload }: ResultItemProps) => {
   const formMethods = useForm({
-    defaultValues: {
-      lancadoEm: null,
-      periodo: null,
-      parcial: false,
-      metaAtingida: false,
-      avaliacao: '',
-      analise: '',
-    },
+    defaultValues: initialPayload
+      ? initialPayload
+      : {
+          lancadoEm: null,
+          periodo: null,
+          parcial: false,
+          metaAtingida: false,
+          avaliacao: '',
+          analise: '',
+          anexos: null,
+        },
+    mode: 'onBlur',
   });
+
+  const metaId = initialPayload?.id;
 
   const { control, setValue, register, formState } = formMethods;
   const field = (field: string) => register(field as any, { required: 'Campo obrigatório' });
@@ -40,89 +46,149 @@ const ResultItem = ({ save }: ResultItemProps) => {
   const periodo = useWatch({ control, name: 'periodo' });
   const lancadoEm = useWatch({ control, name: 'lancadoEm' });
 
-  return (
-    <Box display="flex" flexDirection="column" gap="24px" className="container-style">
-      <Box display="flex" gap="20px" alignContent="flex-start" alignItems="start">
-        {/* TODO: Trocar este datePicker pelo que está criado na branch de Riscos */}
-        <FormControl className="me-2">
-          <DatePicker
-            selected={lancadoEm}
-            onChange={value => setValue('lancadoEm', value, { shouldValidate: true })}
-            dateFormat="dd/MM/yyyy"
-            locale="pt-BR"
-            className="rnc-list-date-picker"
-            placeholderText="Atualizado em"
-          />
-          <label htmlFor="start-date-picker" className="rnc-list-date-label">
-            Data
-          </label>
-        </FormControl>
+  const isDisabled = !!initialPayload?.id;
 
-        <FormControlLabel control={<Switch {...register('parcial')} />} label="parcial" labelPlacement="top" />
-        <FormControlLabel control={<Switch {...register('metaAtingida')} />} label="meta atingida" labelPlacement="top" />
-
-        {/* TODO: Trocar este datePicker pelo que está criado na branch de Riscos */}
-        <FormControl className="me-2">
-          <DatePicker
-            selected={periodo}
-            onChange={value => setValue('periodo', value, { shouldValidate: true })}
-            dateFormat="MMMM / yyyy"
-            locale="pt-BR"
-            className="rnc-list-date-picker"
-            placeholderText="Período"
-            showMonthYearPicker
-          />
-          <label htmlFor="start-date-picker" className="rnc-list-date-label">
-            Período
-          </label>
-        </FormControl>
-      </Box>
-      <TextField multiline rows={3} fullWidth label="Avaliação do Resultado" {...field('avaliacao')} />
-      <TextField multiline rows={3} fullWidth label="Análise Crítica" {...field('analise')} />
-      <Box display="flex" justifyContent="flex-end">
-        <Button disabled={!formState.isValid} variant="contained" onClick={() => save(formMethods)} color="primary">
-          SALVAR
-        </Button>
-      </Box>
-    </Box>
-  );
-};
-
-export const ResultPage = () => {
-  const queryClient = new QueryClient();
-  const navigate = useNavigate();
-  const [isShowingNewForm, setIsShowingNewForm] = useState(false);
-
-  const { idMeta } = useParams();
-
-  const { data: recordd, isLoading: isLoadingRecord } = useQuery(
+  const { data: attatchment, isLoading: isLoadingAttatchment } = useQuery(
     {
-      queryKey: [''],
-      queryFn: () => [],
-      enabled: !!idMeta, // Só faz a consulta se o ID estiver presente
+      queryKey: [`result-attatchment/${metaId}`],
+      queryFn: () => getMetaResultAttatchment(Number(metaId)),
+      enabled: !!metaId,
       staleTime: 60000, // Dados ficam atualizados por 1 minuto,
     },
     queryClient
   );
 
-  const record = [{ id: 2 }];
+  function download() {
+    const link = document.createElement('a');
+    link.href = '/services/all4qmsmsmetaind/api/metaobj/anexos/download/zip/' + initialPayload?.id;
+    link.download = attatchment.nomeFisico;
+
+    // Simula um clique no link para iniciar o download
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+
+  return (
+    <Box display="flex" flexDirection="column" gap="24px" className="container-style">
+      <Box display="flex" gap="20px" alignContent="flex-start" alignItems="start">
+        {/* TODO: Trocar este datePicker pelo que está criado na branch de Riscos */}
+        <FormControl className="me-2">
+          <MaterialDatepicker
+            selected={lancadoEm}
+            onChange={value => setValue('lancadoEm', value, { shouldValidate: true })}
+            dateFormat="dd/MM/yyyy"
+            label="Atualizado em"
+            disabled={isDisabled}
+          />
+        </FormControl>
+
+        <Controller
+          control={control}
+          name="parcial"
+          render={({ field: { value, onChange } }) => (
+            <FormControlLabel
+              control={<Switch disabled={isDisabled} onChange={onChange} checked={value} />}
+              label="parcial"
+              labelPlacement="top"
+            />
+          )}
+        />
+
+        <Controller
+          control={control}
+          name="metaAtingida"
+          render={({ field: { value, onChange } }) => (
+            <FormControlLabel
+              control={<Switch disabled={isDisabled} onChange={onChange} checked={value} />}
+              label="meta atingida"
+              labelPlacement="top"
+            />
+          )}
+        />
+
+        {/* TODO: Trocar este datePicker pelo que está criado na branch de Riscos */}
+        <FormControl className="me-2">
+          <MaterialDatepicker
+            selected={periodo}
+            onChange={value => setValue('periodo', value, { shouldValidate: true })}
+            dateFormat="MMMM/yyyy"
+            label="Período"
+            showMonthYearPicker
+            disabled={isDisabled}
+          />
+        </FormControl>
+
+        <Controller
+          control={control}
+          name={'anexos'}
+          rules={{ required: 'Arquivo(s) obrigatórios' }}
+          render={({ field: { value, onChange, ...field } }) => {
+            return <AttachmentButton {...(initialPayload?.id ? { download } : {})} onChange={onChange} />;
+          }}
+        />
+      </Box>
+      <TextField disabled={isDisabled} multiline rows={3} fullWidth label="Avaliação do Resultado" {...field('avaliacao')} />
+      <TextField disabled={isDisabled} multiline rows={3} fullWidth label="Análise Crítica" {...field('analise')} />
+      {!isDisabled && (
+        <Box display="flex" justifyContent="flex-end">
+          <Button disabled={!formState.isValid} variant="contained" onClick={() => save(formMethods)} color="primary">
+            SALVAR
+          </Button>
+        </Box>
+      )}
+    </Box>
+  );
+};
+
+export const ResultPage = () => {
+  const [isShowingNewForm, setIsShowingNewForm] = useState(false);
+
+  const { metaId } = useParams();
+
+  const { data: meta, isLoading: isLoadingMeta } = useQuery(
+    {
+      queryKey: [`meta/${metaId}`],
+      queryFn: () => getMeta(Number(metaId)),
+      enabled: !!metaId,
+      staleTime: 60000, // Dados ficam atualizados por 1 minuto,
+    },
+    queryClient
+  );
+
+  const { data: results, isLoading: isLoadingResults } = useQuery(
+    {
+      queryKey: [`meta-results/${metaId}`],
+      queryFn: () => getMetaResults(Number(metaId)),
+      enabled: !!metaId, // Só faz a consulta se o ID estiver presente
+      staleTime: 60000, // Dados ficam atualizados por 1 minuto,
+    },
+    queryClient
+  );
 
   const createMutation = useMutation(
     {
       mutationFn: createResult,
       onSuccess: () => {
-        queryClient.invalidateQueries({ queryKey: ['records'] }); // Invalida as queries para atualizar a lista de registros
-        navigate('/records'); // Redireciona para a lista de registros
+        queryClient.invalidateQueries({ queryKey: [`meta-results/${metaId}`] }); // Invalida as queries para atualizar a lista de registros
+        setIsShowingNewForm(false);
+        toast.success('Resultado cadastrado com sucesso');
       },
     },
     queryClient
   );
 
   function saveItem(formMethods: UseFormReturn<any>) {
-    console.log(formMethods.getValues());
-    formMethods.trigger();
-    console.log(formMethods.formState.errors);
-    console.log(formMethods.formState.isValid ? 'Está válido' : 'Não está válido');
+    const { anexos, ...payload } = formMethods.getValues();
+    payload.meta = { id: metaId };
+
+    const formData = new FormData();
+    formData.append('metaResultadoDTO', JSON.stringify(payload));
+    for (const item of anexos as any[]) {
+      formData.append('anexos', item);
+    }
+
+    createMutation.mutate(formData);
   }
 
   return (
@@ -132,14 +198,16 @@ export const ResultPage = () => {
           <Link to={'/'} style={{ textDecoration: 'none', color: '#49a7ea', fontWeight: 400 }}>
             Home
           </Link>
-          <Typography className="link">Metas e Objetivos</Typography>
+          <Link to={'/goals'} style={{ textDecoration: 'none', color: '#49a7ea', fontWeight: 400 }}>
+            Metas e Objetivos
+          </Link>
           <Typography className="link">Resultado</Typography>
         </Breadcrumbs>
         <h1 className="title">Resultado</h1>
-        <TextField fullWidth label="Descrição Ação" multiline rows={3} disabled InputProps={{ readOnly: true }} />
+        <TextField fullWidth label="Descrição Ação" multiline rows={3} value={meta?.acao || ''} disabled />
       </div>
 
-      {record?.length && (
+      {!!results?.length && (
         <Box display="flex" justifyContent="end" position="relative">
           <Fab sx={{ position: 'absolute', top: '195px', marginRight: '15px' }} onClick={() => setIsShowingNewForm(!isShowingNewForm)}>
             {isShowingNewForm ? <DeleteIcon /> : <AddIcon />}
@@ -148,9 +216,9 @@ export const ResultPage = () => {
       )}
 
       <Box display="flex" flexDirection="column" gap="15px" paddingRight="110px" mt="20px">
-        {(isShowingNewForm || !record?.length) && <ResultItem save={saveItem} />}
-        {(record || []).map((field, index) => (
-          <ResultItem save={saveItem} key={field.id} />
+        {(isShowingNewForm || !results?.length) && !isLoadingResults && <ResultItem save={saveItem} />}
+        {(results || []).map((field, index) => (
+          <ResultItem save={saveItem} key={field.id} initialPayload={field} />
         ))}
       </Box>
     </div>
