f1bcebb2f236cbe2f674ffb6457c6b844f40e11f
Erickson Alves <erickson.higor@gmail.com>
2024-10-10 20:18:35 -0400

    fix: Evita replicações de dados existentes
    Signed-off-by: Erickson Alves <erickson.higor@gmail.com>

--- src/main/webapp/app/modules/rnc/reducers/investigation.reducer.ts
+++ src/main/webapp/app/modules/rnc/reducers/investigation.reducer.ts
@@ -2,6 +2,16 @@ import axios from 'axios';
 
 const apiUrl = 'services/all4qmsmsrnc/api/investigacao';
 
+export const deleteInvestigation = async (id: number) => {
+  const response = await axios.delete(`${apiUrl}/${id}`);
+  return response.data;
+};
+
+export const getInvestigations = async () => {
+  const response = await axios.get(apiUrl);
+  return response.data;
+};
+
 export const getInvestigationByRnc = async (id: string | number) => {
   const response = await axios.get(`${apiUrl}/byidnc/${id}`);
   return response.data;
